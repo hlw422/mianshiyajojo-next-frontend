3dae41f2e7b97ccdc78bb4e2fba024df7833c070
hlw <hlw422@126.com>
2024-11-17 21:22:36 +0800

    nextjs 后端代码开发

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Button } from "antd";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
+        <Button type="primary">Primary Button</Button>
+        <Image src={`/assets/logo.png`} alt={"Logo"} width="64" height="64" />
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
